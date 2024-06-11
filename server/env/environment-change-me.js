37fb1323f177e8490ebfbb9696d744067127d7ef
srbuddha <137397737+srbuddha@users.noreply.github.com>
2024-06-11 23:17:21 +0530

    Update environment-change-me.js

--- server/env/environment-change-me.js
+++ server/env/environment-change-me.js
@@ -1,6 +1,6 @@
-const cosmosPort = 1234; // replace with your port
-const dbName = 'your-cosmos-db-name-goes-here';
-const key = 'your-key-goes-here';
+const cosmosPort = 443; // replace with your port
+const dbName = 'CDI_demo';
+const key = '6kkDjtfBiMp3McOmOFMTbSucxZHX98ajbKmrD6Xn1S0zQwMNPQmzkE8sg0PTikdQbw358XVIzv3ZACDbVfH2Eg==';
 
 module.exports = {
   cosmosPort,
